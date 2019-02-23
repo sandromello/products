931c166ffe6e03618eda15cfcf9152f04d8995b5
Sandro  Mello <sandromll@gmail.com>
2019-02-23 18:36:31 -0300

    Fix typos/bugs on templates

--- versioning.mk
+++ versioning.mk
@@ -3,8 +3,6 @@ GIT_TAG ?= $(or ${TRAVIS_TAG},${TRAVIS_TAG},${GITCOMMIT_SHORT})
 VERSION ?= ${GIT_TAG}
 GITCOMMIT ?= $(shell git rev-parse HEAD)
 
-DATE ?= $(shell date -u "+%Y-%m-%dT%H:%M:%SZ")
-
 REGISTRY ?= quay.io
 IMAGE_PREFIX ?= sandromello
 
